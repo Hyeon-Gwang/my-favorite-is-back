09c480055679cf3405d35ddd9cf96713a1ff49ae
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-02-12 00:48:33 +0900

    게시글 수정 샘플 완성

--- routes/post.js
+++ routes/post.js
@@ -25,20 +25,20 @@ router.post("/new", async (req, res, next) => {
       userId: 1,  // test용으로 1번 유저 삽입
     });
 
-    const result = await Promise.all(tags.map(tag => Tag.findOrCreate({
+    const tagCheck = await Promise.all(tags.map(tag => Tag.findOrCreate({
       where: { name: tag },
     }))); // result [ [tag {}, true], [tag {}, false] ]
-    await post.addTags(result.map(tag => tag[0]));
+    await post.addTags(tagCheck.map(tag => tag[0]));
 
     const newPost = await Post.findOne({
       where: { id: post.id },
       attributes: ["id", "title", "imageUrl"],
       include: [{
         model: Tag,
-        attributes: ["name"]
+        attributes: ["name"],
       }, {
         model: User,
-        attributes: ["id", "nickname"]
+        attributes: ["id", "nickname"],
       }]
     })
     
@@ -52,7 +52,37 @@ router.post("/new", async (req, res, next) => {
 // 포스트 수정 PATCH /api/post/3
 router.patch("/:postId", async (req, res, next) => {
   try {
+    const { postId } = req.params;
+    const { title, tags } = req.body;
 
+    const post = await Post.findOne({
+      where: { id: postId },
+      include: {
+        model: Tag
+      }
+    });
+    await post.update({  // 제목 수정
+      title,
+    });
+    await post.removeTags(post.tags.map((tag => tag)));  // 기존 태그 삭제
+
+    const tagCheck = await Promise.all(tags.map(tag => Tag.findOrCreate({
+      where: { name: tag },
+    })));
+    await post.addTags(tagCheck.map(tag => tag[0]));
+
+    const patchedPost = await Post.findOne({
+      where: { id: postId },
+      attributes: ["id", "title", "imageUrl"],
+      include: [{
+        model: Tag,
+        attributes: ["name"],
+      }, {
+        model: User,
+        attributes: ["id", "nickname"],
+      }]
+    })
+    return res.status(200).json(patchedPost);
   } catch(error) {
     console.error(error);
     next(error);
