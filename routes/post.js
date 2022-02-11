167fee592342e6cf82246ecb0360117576ba816c
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-02-11 22:49:20 +0900

    post 라우터 목록만 작성

--- routes/post.js
+++ routes/post.js
@@ -2,10 +2,60 @@ const express = require("express");
 
 const router = express.Router();
 
-// GET /api/post
-router.get("/", async (req, res, next) => {
+// 이미지 업로드 POST /api/post/image
+router.post("/image", async (req, res, next) => {
   try {
-    res.status(200).send("sended from '/api/post'");
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+// 포스트 작성 POST /api/post/new
+router.post("/new", async (req, res, next) => {
+  try {
+    
+  } catch(error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+// 포스트 수정 PATCH /api/post/3
+router.patch("/:postId", async (req, res, next) => {
+  try {
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+// 포스트 삭제 DELETE /api/post/3
+router.delete("/:postId", async (req, res, next) => {
+  try {
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+// 포스트 좋아요 누르기 GET /api/post/3/likes
+router.get("/:postId/likes", async (req, res, next) => {
+  try {
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+// 포스트 좋아요 취소 DELETE /api/post/3/likes
+router.delete("/:postId/likes", async (req, res, next) => {
+  try {
+
   } catch(error) {
     console.error(error);
     next(error);
