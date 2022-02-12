e4d32258fa84bee242ad8bfc58b277d6ba4965e7
unknown <tjddnjs7559@gmail.com>
2022-02-12 15:24:19 +0900

    v1

--- routes/posts.js
+++ routes/posts.js
@@ -8,15 +8,23 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   try {
     // 좋아요 누른 게시글 조회
-  //   const { likes } = req.query;
-  //   const { user } = res.locals;
-  //   const likePosts;
-  //   if (likes==true){
-  //   const likePosts = await Post.findAll({      //해당하는 게시글들을 가져온다.
-  //     where: {Likers : user}           
-  //   })
-  //   res.json({ likePosts });
-  // }
+    const { likes } = req.query;
+    const { user } = res.locals;
+    if (likes==true){
+    const likePosts = await Post.findAll({      //해당하는 게시글들을 가져온다.
+      where: {Likers : user}           
+    })
+    res.json({ likePosts });
+  }
+
+  //특정태그가 달린 게시글 조회
+  const {tags} =req.query;
+  if(tags!==null||tags!==undefined){
+    const selectedTag = await Post.findAll({
+      where: {name : tags }
+    })
+    res.json({selectedTag})
+  }
 
   // 전체 게시글 조회
     const posts = await Post.findAll({});
@@ -52,15 +60,5 @@ router.get("/:postId", async (req, res, next) => {
 
 
 
-// 특정 태그가 달린 게시글 조회
-router.get("/", async (req, res, next) => {
-  try {
-    const posts = await Post.findAll().sort("-createdAt");
-    res.json({ posts });
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
 
 module.exports = router;
