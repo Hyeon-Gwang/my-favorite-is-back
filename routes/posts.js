281eaa769fbf00f9a0ee58a05f154f89ed78cdc5
seongwon <87584033+Magiof@users.noreply.github.com>
2022-02-13 22:40:04 +0900

    posts v1

--- routes/posts.js
+++ routes/posts.js
@@ -7,72 +7,72 @@ const authMiddleware = require("../middlewares/auth-middleware");
 router.get("/", async (req, res) => {
   try {
     const { tag } = req.query;
-    //전체 게시글 조회
+    console.log(tag)
+    //전체 게시글 조회    완성!
     if(!tag||tag===null||tag===undefined) {
-      const posts = await models.Post.findAll({})
-      return res.json({posts})
-    }
-      const TAG = await models.Tag.findAll({
-        where: {name : tag}
+      const posts = await models.Post.findAll({
+        include: [{
+          model: models.Tag,
+          through: { attributes:[]},
+        }]
       })
-      const selectedTag = await TAG.getPostTag.findAll({    //PostTag에서 찾는다
-        where: { name: tag }, //name과 tags가 같은것만
-        include: [
-          {
-            model: models.Post, //post를 가져옴
+      return res.json(posts)
+    }
+    //특정 태그 달린 게시글 조회    완성!
+      const selectedTag = await models.Tag.findOne({
+        where: {name : tag},
+        include: [{
+          model: models.Post,
+          through: { attributes:[]},
+        }],
+      })
+       res.json( selectedTag.posts );
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 게시글 상세 페이지 조회  완성!
+router.get("/detail/:postId", async (req, res) => {        //   /:postId 에서 /detail/:postId로 변경
+  try {
+    const { postId } = req.params;
+    const detail = await models.Post.findOne({
+      where: { id: postId },
+      attributes: ["title", "imageUrl",  "createdAt", "updatedAt"],
+      include: [
+        {
+          model: models.Tag,
+          attributes: ["name"],
+          through: { attributes:[]},
+        },
+        {
+          model: models.User,
+          attributes:["nickname"]
+        },
+      ],
+    });
+    return res.json(detail);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//내가 좋아요 한 게시글만 보기   완성!
+router.get("/likes",authMiddleware, async (req, res) => {        // /likes가 /:postId 에서 먼저 인식되므로 상세 페이지 조회 url을 변경
+  try {
+    const { user } = res.locals;
+    const mine = await models.User.findOne({
+      where: { userID: user[0].dataValues.userID },
+      include:[
+        {
+           model : models.Post,
           },
-        ],
-      });
-       res.json({ selectedTag });
-    
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-// 게시글 상세 페이지 조회
-router.get("/:postId", async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const detail = await models.Post.findOne({
-      where: { id: postId },
-      attributes: ["title", "imageUrl", "userId", "createdAt", "updatedAt"],
-      include: [
-        {
-          model: models.Tag,
-          attributes: ["name"],
-        },
-        {
-          model: models.User,
-          attributes:["nickname"]
-        },
       ],
     });
-    return res.json(detail);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-
-router.get("/likes", async (req, res) => {
-  try {
-    const { postId } = req.params;
-    const detail = await models.Post.findOne({
-      where: { id: postId },
-      attributes: ["title", "imageUrl", "userId", "createdAt", "updatedAt"],
-      include: [
-        {
-          model: models.Tag,
-          attributes: ["name"],
-        },
-        {
-          model: models.User,
-          attributes:["nickname"]
-        },
-      ],
+    const myLikes = await mine.getLiked({
+      attributes: ["id", "title", "imageUrl", "createdAt"],
     });
-    return res.json(detail);
+    return res.json(myLikes);
   } catch (error) {
     console.error(error);
   }
