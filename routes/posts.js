946bc6db643616aac242397b5f63920179c4a1d5
seongwon <87584033+Magiof@users.noreply.github.com>
2022-02-13 22:52:15 +0900

    posts v1

--- routes/posts.js
+++ routes/posts.js
@@ -7,47 +7,55 @@ const authMiddleware = require("../middlewares/auth-middleware");
 router.get("/", async (req, res) => {
   try {
     const { tag } = req.query;
-    console.log(tag)
-    //전체 게시글 조회    완성!
-    if(!tag||tag===null||tag===undefined) {
+    console.log(tag);
+    
+    //전체 게시글 조회
+    if (!tag || tag === null || tag === undefined) {
       const posts = await models.Post.findAll({
-        include: [{
-          model: models.Tag,
-          through: { attributes:[]},
-        }]
-      })
-      return res.json(posts)
+        attributes: ["id", "title", "imageUrl", "createdAt"],
+        include: [
+          {
+            model: models.Tag,
+            attributes: ["name"],
+            through: { attributes: [] },
+          },
+        ],
+      });
+      return res.json(posts);
     }
-    //특정 태그 달린 게시글 조회    완성!
-      const selectedTag = await models.Tag.findOne({
-        where: {name : tag},
-        include: [{
+
+    //특정 태그 달린 게시글 조회
+    const selectedTag = await models.Tag.findOne({
+      where: { name: tag },
+      include: [
+        {
           model: models.Post,
-          through: { attributes:[]},
-        }],
-      })
-       res.json( selectedTag.posts );
+          through: { attributes: [] },
+        },
+      ],
+    });
+    res.json(selectedTag.posts);
   } catch (error) {
     console.error(error);
   }
 });
 
-// 게시글 상세 페이지 조회  완성!
-router.get("/detail/:postId", async (req, res) => {        //   /:postId 에서 /detail/:postId로 변경
+// 게시글 상세 페이지 조회
+router.get("/detail/:postId", async (req, res) => {             //   /:postId 에서 /detail/:postId로 변경
   try {
     const { postId } = req.params;
     const detail = await models.Post.findOne({
       where: { id: postId },
-      attributes: ["title", "imageUrl",  "createdAt", "updatedAt"],
+      attributes: ["title", "imageUrl", "createdAt", "updatedAt"],
       include: [
         {
           model: models.Tag,
           attributes: ["name"],
-          through: { attributes:[]},
+          through: { attributes: [] },
         },
         {
           model: models.User,
-          attributes:["nickname"]
+          attributes: ["nickname"],
         },
       ],
     });
@@ -57,16 +65,16 @@ router.get("/detail/:postId", async (req, res) => {        //   /:postId 에서 
   }
 });
 
-//내가 좋아요 한 게시글만 보기   완성!
-router.get("/likes",authMiddleware, async (req, res) => {        // /likes가 /:postId 에서 먼저 인식되므로 상세 페이지 조회 url을 변경
+//내가 좋아요 한 게시글만 보기
+router.get("/likes", authMiddleware, async (req, res) => {          // /likes가 /:postId 에서 먼저 인식되므로 상세 페이지 조회 url을 변경
   try {
     const { user } = res.locals;
     const mine = await models.User.findOne({
       where: { userID: user[0].dataValues.userID },
-      include:[
+      include: [
         {
-           model : models.Post,
-          },
+          model: models.Post,
+        },
       ],
     });
     const myLikes = await mine.getLiked({
@@ -77,7 +85,5 @@ router.get("/likes",authMiddleware, async (req, res) => {        // /likes가 /:
     console.error(error);
   }
 });
-
-
 
 module.exports = router;
